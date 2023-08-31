ccd86f2adcde3fee983ebbca3825a980e20a6174
shprince1 <princehsylvanus@gmail.com>
2023-08-31 02:56:40 +0100

    delete button added

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const screenDisplay = document.querySelector('.screen')
 const buttons = document.querySelectorAll('button')
 // console.log(buttons)
 
+
+
+// backspace
+function back() {
+    var value = document.getElementById("d").value;
+    document.getElementById("d").value = value.substr(0, value.length - 1);
+}
+
+
+// ;;;;;;;
 let calculation =[]
-let accumulativeCalculation
+let accumulativeCalculation=[]
 
 function calculate(button){
     // console.log(button)
     const value = button.textContent
     if( value =='CLEAR'){
         calculation =[]
-        screenDisplay.textContent='.'
+        screenDisplay.textContent='0'
     }else if(value=== '='){
         screenDisplay.textContent=  eval(accumulativeCalculation)
 
-    }else {
+
+    }else if(value=='DEL'){
+        calculation.pop(value)
+        screenDisplay.textContent=screenDisplay.textContent.slice(0, -1,);
+    }
+    else {
         calculation.push(value)
     accumulativeCalculation = calculation.join('')
     screenDisplay.textContent =accumulativeCalculation
