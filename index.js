7d63d470738c88d2ea15e0b73cc71806a44b9fc9
shprince1 <princehsylvanus@gmail.com>
2023-08-31 01:46:19 +0100

    firsts commits

---
+++ index.js
@@ -0,0 +1,34 @@
+const screenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+// console.log(buttons)
+
+let calculation =[]
+let accumulativeCalculation
+
+function calculate(button){
+    // console.log(button)
+    const value = button.textContent
+    if( value =='CLEAR'){
+        calculation =[]
+        screenDisplay.textContent='.'
+    }else if(value=== '='){
+        screenDisplay.textContent=  eval(accumulativeCalculation)
+
+    }else {
+        calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent =accumulativeCalculation
+    }
+
+
+
+    // calculation.push(value)
+    // accumulativeCalculation = calculation.join('')
+    // screenDisplay.textContent =accumulativeCalculation
+
+    // console.log(calculation )
+
+
+}
+
+buttons.forEach(button => button.addEventListener('click',()=>calculate(button) ))
